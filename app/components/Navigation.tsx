ea99cb9fb4117218e73d02f99b2f028e5f1fefb5
Hafaiedh Akram <hfaiedhakram@gmail.com>
2025-10-28 20:18:20 +0100

    testing another icon

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -30,7 +30,7 @@ export default function Navigation() {
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
                     {/* <div className="text-xl font-bold text-slate-800 dark:text-white">AK</div> */}
-                    <Image src="/web-app-manifest-512x512.png" alt="Logo" width={50} height={50} />
+                    <Image src="/icon0.svg" alt="Logo" width={50} height={50} />
 
 
                     {/* Desktop Navigation */}
