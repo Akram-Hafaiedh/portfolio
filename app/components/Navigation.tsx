d2a0594dde8ce5c054caee2d332a5b142364e639
Hafaiedh Akram <hfaiedhakram@gmail.com>
2025-10-28 20:12:12 +0100

    testing icon on Navbar

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import ThemeToggle from './ThemeToggle';
+import Image from 'next/image';
 
 export default function Navigation() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -28,7 +29,9 @@ export default function Navigation() {
         <nav className="fixed top-0 w-full bg-white/80 dark:bg-slate-900/80 backdrop-blur-sm z-50 border-b border-slate-200 dark:border-slate-700">
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
-                    <div className="text-xl font-bold text-slate-800 dark:text-white">AK</div>
+                    {/* <div className="text-xl font-bold text-slate-800 dark:text-white">AK</div> */}
+                    <Image src="/web-app-manifest-512x512.png" alt="Logo" width={50} height={50} />
+
 
                     {/* Desktop Navigation */}
                     <div className="hidden md:flex items-center space-x-8">
